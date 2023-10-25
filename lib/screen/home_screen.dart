a57cb4abeb5dfb42bcbb89e4f2edf81cd00b1092
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2023-10-25 10:19:09 +0600

    splash-screen

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Categories",
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,7 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:sms_buddy/screen/home_screen.dart';
+import 'package:sms_buddy/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,20 +16,51 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Categories",
-          style: TextStyle(fontSize: 28, color: Colors.black),
-        ),
-      ),
       body: Padding(
         padding: EdgeInsets.all(12),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-             Lottie.asset('assets/json/splash_lottie.json'),
+            SizedBox(
+              height: size.height * 0.3,
+            ),
+            Lottie.asset('assets/json/splash_lottie.json',
+                width: size.width, height: size.height * 0.5),
+            Column(
+              children: [
+                Text(
+                  Utils.appName,
+                  style: TextStyle(
+                      fontSize: 30,
+                      letterSpacing: 1,
+                      color: Colors.green.shade700,
+                      fontWeight: FontWeight.w800,
+                      fontStyle: FontStyle.italic),
+                ),
+                Text(
+                  "Version: 1.0",
+                  style: TextStyle(
+                      fontSize: 20,
+                      letterSpacing: 1,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w800,
+                      fontStyle: FontStyle.italic),
+                )
+              ],
+            )
           ],
         ),
       ),
